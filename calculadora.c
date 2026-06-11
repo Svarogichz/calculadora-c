f37fb9620810f9af6c88de2df20b063afc047e40
Diego Salas <diego.salasg@live.com>
2026-06-10 19:44:45 -0600

    Se agrego suma y resta

--- calculadora.c
+++ calculadora.c
@@ -7,5 +7,17 @@ float division(float,float);
 
 int main() {
     printf("Calculadora sencilla\n");
+    suma();
+    printf("Resultado resta: %.2f\n", resta(10,5));
     return 0;
+}
+void suma() {
+    float a, b;
+    printf("Ingresa dos numeros para sumar: ");
+    scanf("%f %f", &a, &b);
+    printf("Resultado suma: %.2f\n", a + b);
+}
+
+float resta(float a, float b) {
+    return a - b;
 }
