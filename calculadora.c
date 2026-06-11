dba82af0aeee72bc9bc2a9f2893e3d3e88d0ef87
Diego Salas <diego.salasg@live.com>
2026-06-10 19:03:05 -0600

    Codigo base para la calculadora

---
+++ calculadora.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void suma();
+float resta(float,float);
+void multiplicacion();
+float division(float,float);
+
+int main() {
+    printf("Calculadora sencilla\n");
+    return 0;
+}
