1df1ebc21df2f5cdee33c3866ddcffb01bf1f44f
Christian Ortega <Christian.zcvz@gmail.com>
2026-06-11 19:32:38 -0600

    Agregadas funciones de multiplicacion y division

--- calculadora.c
+++ calculadora.c
@@ -2,12 +2,16 @@
 
 void suma();
 void resta();
+void multiplicacion();
+void division();
 
 int main() {
-    printf("Calculadora sencilla\n");
+    printf("Calculadora sencilla\n\n");
 
     suma();
     resta();
+    multiplicacion();
+    division();
 
     return 0;
 }
@@ -16,12 +20,32 @@ void suma() {
     float a, b;
     printf("Ingresa dos numeros para sumar: ");
     scanf("%f %f", &a, &b);
-    printf("Resultado suma: %.2f\n", a + b);
+    printf("Resultado suma: %.2f\n\n", a + b);
 }
 
 void resta() {
     float a, b;
     printf("Ingresa dos numeros para restar: ");
     scanf("%f %f", &a, &b);
-    printf("Resultado resta: %.2f\n", a - b);
+    printf("Resultado resta: %.2f\n\n", a - b);
+}
+
+void multiplicacion() {
+    float a, b;
+    printf("Ingresa dos numeros para multiplicar: ");
+    scanf("%f %f", &a, &b);
+    printf("Resultado multiplicacion: %.2f\n\n", a * b);
+}
+
+void division() {
+    float a, b;
+    printf("Ingresa dos numeros para dividir: ");
+    scanf("%f %f", &a, &b);
+
+    if (b == 0) {
+        printf("Error: no se puede dividir entre cero.\n\n");
+        return;
+    }
+
+    printf("Resultado division: %.2f\n\n", a / b);
 }
