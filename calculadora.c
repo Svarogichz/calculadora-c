e05ddd6d5977bc1228ef4ebddc4cc983995410e0
Svarogichz <87203267+Svarogichz@users.noreply.github.com>
2026-06-11 19:38:27 -0600

    original code
    Refactor calculator functions to use float parameters and return types.

--- calculadora.c
+++ calculadora.c
@@ -1,27 +1,11 @@
 #include <stdio.h>
 
 void suma();
-void resta();
+float resta(float, float);
+void multiplicacion();
+float division(float, float);
 
 int main() {
     printf("Calculadora sencilla\n");
-
-    suma();
-    resta();
-
     return 0;
-}
-
-void suma() {
-    float a, b;
-    printf("Ingresa dos numeros para sumar: ");
-    scanf("%f %f", &a, &b);
-    printf("Resultado suma: %.2f\n", a + b);
-}
-
-void resta() {
-    float a, b;
-    printf("Ingresa dos numeros para restar: ");
-    scanf("%f %f", &a, &b);
-    printf("Resultado resta: %.2f\n", a - b);
 }
